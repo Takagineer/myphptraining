0e6a0063d77784c76ed68a6061558c5fdef7d2f9
Takagineer <itengineer0415@gmail.com>
2022-05-10 21:13:05 +0900

    update/selectで選択したものを表示

--- work/web/index.php
+++ work/web/index.php
@@ -10,7 +10,12 @@ include('../app/_parts/_header.php');
 <form action="result.php" method="get">
   <!-- <input type="text" name="message"> -->
   <!-- <input type="text" name="username"> -->
-  <textarea name="message"></textarea>
+  <!-- <textarea name="message"></textarea> -->
+  <select name="color" >
+    <option value="orange">オレンジ</option>
+    <option value="pink">ピンク</option>
+    <option value="gold">ゴールド</option>
+  </select>
   <button>send</button>
 </form>
 
--- work/web/result.php
+++ work/web/result.php
@@ -4,13 +4,15 @@
 require('../app/functions.php');
 include('../app/_parts/_header.php');
 
-$message = trim(filter_input(INPUT_GET,"message"));
+// $message = trim(filter_input(INPUT_GET,"message"));
 // $username = filter_input(INPUT_GET,"username");
-$message = $message !== ''? $message : '...';
+// $message = $message !== ''? $message : '...';
+
+$color = filter_input(INPUT_GET,"color")
 
 ?>
 
-<?= nl2br(h($message)); ?>
+<?= nl2br(h($color)); ?>
 <!-- <?= h($username); ?></p> -->
 <p><a href="index.php">Go back</a></p>
 

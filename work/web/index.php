f6803c84cb60e82fe5a8cc81bb73acd113bb4364
Takagineer <itengineer0415@gmail.com>
2022-05-11 22:56:10 +0900

    update/CSRF対策

--- work/web/index.php
+++ work/web/index.php
@@ -3,9 +3,13 @@
 
 require('../app/functions.php');
 
+createToken();
+
 define('FILENAME' , '../app/messages.txt');
 
 if($_SERVER['REQUEST_METHOD']=== "POST"){
+  validateToken();
+  
   $message = trim(filter_input(INPUT_POST,"message"));
   $message = $message !==''? $message : '...';
   
@@ -34,6 +38,7 @@ include('../app/_parts/_header.php');
 <form action="" method="post">
   <input type="text" name="message">
   <button>post</button>
+  <input type="hidden" name="token" value="<?= h($_SESSION['token']) ?>" >
 </form>
 
 <?php
